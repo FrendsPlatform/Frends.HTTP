13c52fb9690d4f2b4af9e0026700721d041f80b7
Vertti Ahlstén <vertti.ahlsten@hiq.fi>
2023-01-13 14:26:20 +0200

    Initial implementation

---
+++ Frends.HTTP.Request/Frends.HTTP.Request.Tests/UnitTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Frends.HTTP.Request.Definitions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Frends.HTTP.Request.Tests;
+
+public class UnitTests
+{
+    [TestClass]
+    public class HTTPRequestTests
+    {
+        /// <summary>
+        /// Setup MySQL to docker:
+        /// docker run -p 3306:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql
+        /// </summary>
+
+        [TestInitialize]
+        public async Task OneTimeSetUp()
+        {
+
+        }
+
+        [TestCleanup]
+        public async Task OneTimeTearDown()
+        {
+
+        }
+
+        [TestMethod]
+        public async Task TestCallStoredProcedure()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ Frends.HTTP.Request/Frends.HTTP.Request/Request.cs
@@ -0,0 +1,30 @@
+﻿using Frends.HTTP.Request.Definitions;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Frends.HTTP.Request;
+
+/// <summary>
+/// Task class.
+/// </summary>
+public class HTTP
+{
+    /// <summary>
+    /// Execute a stored procedure to MySQL.
+    /// [Documentation](https://tasks.frends.com/tasks#frends-tasks/Frends.HTTP.Request)
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="options"></param>
+    /// <param name="cancellationToken"/>
+    /// <returns>Object { int AffectedRows }</returns>
+    public static async Task<Result> Request(
+        [PropertyTab] Input input,
+        [PropertyTab] Options options,
+        CancellationToken cancellationToken
+    )
+    {
+        
+    }
+
+}
