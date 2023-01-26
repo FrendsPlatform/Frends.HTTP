ef1875c5f80b9816152de58bfa68ef66f0acbf91
helvertti <vertti.ahlsten@hiq.fi>
2023-01-26 10:33:54 +0200

    Helpers moved to own classes

---
+++ Frends.HTTP.Request/Frends.HTTP.Request/Definitions/HttpClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+
+namespace Frends.HTTP.Request.Definitions;
+
+internal class HttpClientFactory : IHttpClientFactory
+{
+    public HttpClient CreateClient(Options options)
+    {
+        var handler = new HttpClientHandler();
+        handler.SetHandlerSettingsBasedOnOptions(options);
+        return new HttpClient(handler);
+    }
+}
