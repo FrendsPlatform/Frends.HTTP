8c1f4ef9a800afbced4668d195c6a046678bac4b
Riku Virtanen <virtanen.riku91@gmail.com>
2023-05-24 15:46:29 +0300

    Initial implementation

---
+++ Frends.HTTP.SendBytes/Frends.HTTP.SendBytes/Definitions/Result.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace Frends.HTTP.SendBytes.Definitions;
+
+/// <summary>
+/// Result class
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// Body of response
+    /// </summary>
+    /// <example>{"id": "abcdefghijkl123456789",  "success": true,  "errors": []}</example>
+    public string Body { get; private set; }
+
+    /// <summary>
+    /// Headers of response
+    /// </summary>
+    /// <example>{[ "content-type": "application/json", ... ]}</example>
+    public Dictionary<string, string> Headers { get; private set; }
+
+    /// <summary>
+    /// Statuscode of response
+    /// </summary>
+    /// <example>200</example>
+    public int StatusCode { get; private set; }
+
+    internal Result(string body, Dictionary<string, string> headers, int statusCode)
+    {
+        Body = body;
+        Headers = headers;
+        StatusCode = statusCode;
+    }
+}
