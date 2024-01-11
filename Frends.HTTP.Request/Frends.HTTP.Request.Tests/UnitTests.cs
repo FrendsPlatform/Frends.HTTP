896d9d91bbd456efb4fe7128513e7f66f573c782
Riku Virtanen <virtanen.riku91@gmail.com>
2024-01-11 15:47:44 +0200

    Debug

--- Frends.HTTP.Request/Frends.HTTP.Request.Tests/UnitTests.cs
+++ Frends.HTTP.Request/Frends.HTTP.Request.Tests/UnitTests.cs
@@ -134,7 +134,6 @@ public class UnitTests
         string user = RandomString(3);
         string pass = RandomString(3);
         string expectedReturn = $"'{user}{pass}'";
-        
 
         var input = new Input
         {
@@ -371,7 +370,7 @@ public class UnitTests
     private static string RandomString(int length)
     {
         Random random = new Random();
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         return new string(Enumerable.Repeat(chars, length)
             .Select(s => s[random.Next(s.Length)]).ToArray());
     }
