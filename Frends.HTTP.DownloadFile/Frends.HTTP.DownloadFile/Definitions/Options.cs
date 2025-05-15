6f7f580246dd2f7b206a2043bbe7a06a5148e67f
MichalFrends1 <michal.nowakowski@frends.com>
2025-05-15 12:10:19 +0200

    check workflow

--- Frends.HTTP.DownloadFile/Frends.HTTP.DownloadFile/Definitions/Options.cs
+++ Frends.HTTP.DownloadFile/Frends.HTTP.DownloadFile/Definitions/Options.cs
@@ -129,7 +129,7 @@ public class Options
     public bool AutomaticCookieHandling { get; set; } = true;
 
     /// <summary>
-    /// If set to true, an existing file at the target path will be overwritten during download.
+    /// If set to true, an existing file at the target path will be overwritten during download
     /// <example>false</example>
     [DefaultValue(false)]
     public bool Overwrite { get; set; } = false;
