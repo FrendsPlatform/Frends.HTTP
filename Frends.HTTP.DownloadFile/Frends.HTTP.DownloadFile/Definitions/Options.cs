1b8ac3e6761585fb9b95c51c11a4fa854529b142
MichalFrends1 <michal.nowakowski@frends.com>
2025-05-15 10:01:41 +0200

    Added overwrite parameter

--- Frends.HTTP.DownloadFile/Frends.HTTP.DownloadFile/Definitions/Options.cs
+++ Frends.HTTP.DownloadFile/Frends.HTTP.DownloadFile/Definitions/Options.cs
@@ -127,4 +127,11 @@ public class Options
     /// <example>true</example>
     [DefaultValue(true)]
     public bool AutomaticCookieHandling { get; set; } = true;
+
+    /// <summary>
+    /// If set to true, an existing file at the target path will be overwritten during download.
+    /// <example>false</example>
+    [DefaultValue(false)]
+    public bool Overwrite { get; set; } = false;
+
 }
