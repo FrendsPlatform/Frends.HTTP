d74a35cccd210355cf25eb19ae63855aa12f466f
MichalFrends1 <michal.nowakowski@frends.com>
2026-05-06 09:17:12 +0200

    add SslProtocolVersion and HttpProtocolVersion to cache key

--- Frends.HTTP.Request/Frends.HTTP.Request/Request.cs
+++ Frends.HTTP.Request/Frends.HTTP.Request/Request.cs
@@ -254,7 +254,8 @@ public static class HTTP
                + $":{options.ClientCertificateFilePath}:{options.ClientCertificateInBase64}:{options.ClientCertificateKeyPhrase}"
                + $":{options.CertificateThumbprint}:{options.LoadEntireChainForCertificate}:{options.ConnectionTimeoutSeconds}"
                + $":{options.FollowRedirects}:{options.AllowInvalidCertificate}:{options.AllowInvalidResponseContentTypeCharSet}"
-               + $":{options.ThrowExceptionOnErrorResponse}:{options.AutomaticCookieHandling}";
+               + $":{options.ThrowExceptionOnErrorResponse}:{options.AutomaticCookieHandling}"
+               + $":{options.SslProtocolVersion}:{options.HttpProtocolVersion}";
     }
 
     private static async Task<HttpResponseMessage> GetHttpRequestResponseAsync(
